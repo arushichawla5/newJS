8d1d56426fbca7a45fe650f0993e35e9ca150b34
arushi <aarushichawla54@gmail.com>
2026-02-04 18:46:42 +0530

    variable

---
+++ datatype.js
@@ -0,0 +1,15 @@
+let name="histesh"
+let age=21
+let isLoggegIn=false
+//number => 2 to powe 53
+//bigint
+//string => ""
+//null => standalone value empty
+//undefined => variable but no value assigned
+//symbol => unique
+//object => key value pair
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggegIn)  
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountId=144553; //cant change
+
+let accountEmail="hitesh@google.com"; //can change
+
+var accountPass="12345"; //prefer not to use this because of issue in block scope and functional scope
+
+accountCity="jaipur"; //do not use this 
+// accountId=2;
+ accountEmail="hitesh23@google.com";
+ accountPass="123";
+ accountCity="banglore";
+// console.log(accountEmail);
+// console.log(accountPass)
+// console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPass,accountCity])
