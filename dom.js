96680601107ef771d7562b9ce0e62ad691dd7840
arushi <aarushichawla54@gmail.com>
2026-02-09 16:25:36 +0530

    sixth commit

---
+++ dom.js
@@ -0,0 +1,15 @@
+const parent=document.querySelector('.parent')
+// console.log(parent)
+// console.log(parent.children)
+// console.log(parent.children[1].innerText)
+
+// for(let i=0;i<parent.children.length;i++){
+//     console.log(parent.children[i].innerHTML)
+// }
+
+parent.children[1].style.color="orange"
+console.log(parent.firstElementChild)
+console.log(parent.lastElementChild)
+
+const dayOne=document.querySelector('.day')
+console.log(dayOne)
--- form.js
+++ form.js
@@ -1,3 +1,12 @@
-if(input == " "){
-    alert("please fill the form!");
-}
+const myform=document.getElementById("form")
+myform.addEventListener("submit",function(){
+        const formData={
+            name:document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            number: document.getElementById("num").value,
+            msg: document.getElementById("msg").value
+        }
+        localStorage.setItem("formData",JSON.stringify(formData))
+        
+})
+ 
